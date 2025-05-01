798917e37e41a38935f020b8975054a73cb3ac80
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-04-30 22:52:22 -0700

    The offset shouldn't be necessary!

--- core/src/main/java/gdx/liftoff/util/VoxelCollider.java
+++ core/src/main/java/gdx/liftoff/util/VoxelCollider.java
@@ -17,7 +17,6 @@ public class VoxelCollider<T extends HasPosition3D> {
     public Array<T> entities;
 
     private final Vector3 tempVector3 = new Vector3();
-    private final Vector3 tempVector3Offset = new Vector3();
 
     public final Array<T> colliding = new Array<>();
 
@@ -25,9 +24,9 @@ public class VoxelCollider<T extends HasPosition3D> {
         entities = new Array<>(colliders);
     }
 
-    private final Comparator<T> xDistance = (a, b) -> Float.compare(Math.abs(tempVector3Offset.x - a.getPosition().x), Math.abs(tempVector3Offset.x - b.getPosition().x));
-    private final Comparator<T> yDistance = (a, b) -> Float.compare(Math.abs(tempVector3Offset.y - a.getPosition().y), Math.abs(tempVector3Offset.y - b.getPosition().y));
-    private final Comparator<T> zDistance = (a, b) -> Float.compare(Math.abs(tempVector3Offset.z - a.getPosition().z), Math.abs(tempVector3Offset.z - b.getPosition().z));
+    private final Comparator<T> xDistance = (a, b) -> Float.compare(Math.abs(tempVector3.x - a.getPosition().x), Math.abs(tempVector3.x - b.getPosition().x));
+    private final Comparator<T> yDistance = (a, b) -> Float.compare(Math.abs(tempVector3.y - a.getPosition().y), Math.abs(tempVector3.y - b.getPosition().y));
+    private final Comparator<T> zDistance = (a, b) -> Float.compare(Math.abs(tempVector3.z - a.getPosition().z), Math.abs(tempVector3.z - b.getPosition().z));
 
     /**
      * Gets the Array of T entities that collide with the given Vector3 position.
@@ -39,12 +38,11 @@ public class VoxelCollider<T extends HasPosition3D> {
     public Array<T> collisionsWith(T collider) {
         Vector3 voxelPosition = collider.getPosition();
         tempVector3.set(voxelPosition);
-        tempVector3Offset.set(voxelPosition).add(0.5f);
         colliding.clear();
         entities.sort(xDistance);
-        float c = tempVector3Offset.x;
+        float c = tempVector3.x;
         for (T e : entities) {
-            if(Math.abs(c - e.getPosition().x) < 1.5f){
+            if(Math.abs(c - e.getPosition().x) < 1f){
                 // The distance between x coordinates is close enough for them to be overlapping
                 colliding.add(e);
             } else {
@@ -55,19 +53,19 @@ public class VoxelCollider<T extends HasPosition3D> {
         colliding.removeValue(collider, true);
         if(colliding.isEmpty()) return colliding;
         colliding.sort(yDistance);
-        c = tempVector3Offset.y;
+        c = tempVector3.y;
         for (Array.ArrayIterator<T> iterator = colliding.iterator(); iterator.hasNext(); ) {
             T e = iterator.next();
-            if (Math.abs(c) - e.getPosition().y >= 1.5f) {
+            if (Math.abs(c) - e.getPosition().y >= 1f) {
                 iterator.remove();
             }
         }
         if(colliding.isEmpty()) return colliding;
         colliding.sort(zDistance);
-        c = tempVector3Offset.z;
+        c = tempVector3.z;
         for (Array.ArrayIterator<T> iterator = colliding.iterator(); iterator.hasNext(); ) {
             T e = iterator.next();
-            if (Math.abs(c) - e.getPosition().z >= 1.5f) {
+            if (Math.abs(c) - e.getPosition().z >= 1f) {
                 iterator.remove();
             }
         }
