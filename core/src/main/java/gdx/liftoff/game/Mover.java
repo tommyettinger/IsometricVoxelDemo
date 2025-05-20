e32df90ef768232625da41a94253ce7192b0f05a
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-05-19 21:18:40 -0700

    Mover will need... more docs.

--- core/src/main/java/gdx/liftoff/game/Mover.java
+++ core/src/main/java/gdx/liftoff/game/Mover.java
@@ -14,34 +14,92 @@ import gdx.liftoff.LocalMap;
 import gdx.liftoff.util.HasPosition3D;
 import gdx.liftoff.util.MiniNoise;
 
+/**
+ * A creature, hero, or hazard that can move around of its own accord.
+ * A Mover can be an {@link #npc} or not; NPCs move on their own in somewhat-random paths, while the player character
+ * should be the only Mover with {@code npc = false}, and should be moved by the player's input.
+ */
 public class Mover implements HasPosition3D {
+    /**
+     * Can be retrieved with {@link #getPosition()}, which satisfies our interface requirement.
+     */
     private final Vector3 position = new Vector3();
+    /**
+     * The direction this Mover is... moving in. Carries over between frames.
+     */
     public final Vector3 velocity = new Vector3(0, 0, 0);
+    /**
+     * Used to make queries to {@link LocalMap#everything}, when we don't know if a position has anything at it.
+     */
     private final Vector4 tempVectorA = new Vector4();
+    /**
+     * While "grounded" this Mover is stepping on the ground, preventing it from falling and allowing it to jump.
+     */
     public boolean isGrounded;
+    /**
+     * The unique identifier for this Mover; this is usually a sequential int starting at 1, where 1 is the player only.
+     */
     public final int id;
 
+    /**
+     * If true, this Mover will move on its own; if false, it depends on player input to move.
+     */
     public final boolean npc;
 
+    /**
+     * Each Mover knows what map it is on, and uses this to check for collisions with terrain and other Movers.
+     */
     private transient LocalMap map;
 
+    /**
+     * Typically calculated at game start in the main game class, this is an Array of four groups of sprite Animations,
+     * with each group referring to a different facing ({@link #currentDirection}) and whether it is attacking.
+     */
     private final Array<Array<Animation<TextureAtlas.AtlasSprite>>> animations;
-    public final int animationID;
+    /**
+     * The index into the inner Arrays of {@link #animations}, determining which creature this appears as.
+     */
+    public final int animationIndex;
+    /**
+     * Goes up until it reaches a threshold, then physics steps run a number of times based on how much the accumulator
+     * went past that threshold. A common concept in game physics.
+     */
     private transient float accumulator;
+    /**
+     * The amount of time this Mover has been on-screen and able to move. Used for both the invincibility flash for the
+     * player when damaged, and to determine the meandering path of NPCs.
+     */
     private transient float totalMoveTime = 0f;
+    /**
+     * If the player is invincible due to just taking damage, this will be greater than {@link #totalMoveTime}.
+     */
     private transient float invincibilityEndTime = -100f;
+    /**
+     * An int denoting the current facing direction of the sprite and whether it is attaking. 0 and 1 are facing down
+     * and up, respectively, without attacking, and 2 and 3 are down and up, respectively, while attacking.
+     */
     private int currentDirection;
 
+    /**
+     * This only matters for the player currently, but it determines how many times they can take damage.
+     */
     public int health = 3;
 
+    // These constants are hard to adjust, but can be changed to fit your game, very carefully.
     private static final float GRAVITY = -0.04f;
     private static final float MAX_GRAVITY = -0.3f;
     private static final float JUMP_FORCE = 0.6f;
     public static final float MOVE_SPEED = 0.15f;
     public static final float NPC_MOVE_SPEED = 0.07f;
 
+    /**
+     * Goes up every time an {@link #id} needs to be assigned, and is reset to 0 if the map resets.
+     */
     public static int ID_COUNTER = 1;
 
+    /**
+     * The animated sprite used as the visual representation of this Mover; also has an isometric tile position.
+     */
     public AnimatedIsoSprite visual;
 
     public Mover(LocalMap map, Array<Array<Animation<TextureAtlas.AtlasSprite>>> animations, int playerId,
@@ -50,7 +108,7 @@ public class Mover implements HasPosition3D {
         this.position.set(fPos, gPos, hPos);
         this.accumulator = 0;
         this.currentDirection = 0; // Default: facing down
-        this.animationID = playerId;
+        this.animationIndex = playerId;
 
         this.animations = animations;
 
@@ -84,9 +142,9 @@ public class Mover implements HasPosition3D {
             // while jumping, show attack animation; while standing, show idle animation. NPCs are always attacking.
             if (npc || velocity.z != 0) {
                 /* The "currentDirection + 2" gets an attack animation instead of an idle one for the appropriate facing. */
-                visual.animation = animations.get(currentDirection + 2).get(animationID);
+                visual.animation = animations.get(currentDirection + 2).get(animationIndex);
             } else {
-                visual.animation = animations.get(currentDirection).get(animationID);
+                visual.animation = animations.get(currentDirection).get(animationIndex);
             }
 
             visual.setPosition(position);
@@ -378,6 +436,6 @@ public class Mover implements HasPosition3D {
 
     @Override
     public String toString() {
-        return "Mover { visual: " +  visual + ", type: " + AssetData.ENTITIES.findKey(animationID) + " }";
+        return "Mover { visual: " +  visual + ", type: " + AssetData.ENTITIES.findKey(animationIndex) + " }";
     }
 }
--- core/src/main/java/gdx/liftoff/util/HasPosition3D.java
+++ core/src/main/java/gdx/liftoff/util/HasPosition3D.java
@@ -2,6 +2,14 @@ package gdx.liftoff.util;
 
 import com.badlogic.gdx.math.Vector3;
 
+/**
+ * A simple interface for anything that can have its position requested as a Vector3. This usually means an isometric
+ * tile position here, but could be another 3D position in other code. This is used mainly by {@link VoxelCollider}.
+ */
 public interface HasPosition3D {
+    /**
+     * Gets the position of this object as a Vector3
+     * @return this object's Vector3 position, which should be a direct reference for if code needs to modify it
+     */
     Vector3 getPosition();
 }
