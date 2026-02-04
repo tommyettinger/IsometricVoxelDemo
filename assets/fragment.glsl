858e5977ec2ca51f1837d8a185fd09c1b7eb76e6
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-03 18:30:56 -0800

    Using the original v2len works better.
    Using distance() produced one float which got copied into both x and y, where v2len() can produce different x and y.

--- assets/fragment.glsl
+++ assets/fragment.glsl
@@ -10,9 +10,13 @@ varying LOWP vec4 v_color;
 uniform sampler2D u_texture;
 uniform vec2 u_textureResolution;
 
+vec2 v2len(vec2 a, vec2 b) {
+    return sqrt(a*a+b*b);
+}
+
 void main() {
     vec2 uv = v_texCoords * u_textureResolution;
     vec2 seam = floor(uv+.5);
-    uv = seam + clamp((uv-seam)/distance(dFdx(uv),dFdy(uv)), -.5, .5);
+    uv = seam + clamp((uv-seam)/v2len(dFdx(uv),dFdy(uv)), -.5, .5);
     gl_FragColor = texture2D(u_texture, uv/u_textureResolution) * v_color;
 }
